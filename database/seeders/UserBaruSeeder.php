67de2090da1ffc302ec25a6c435c134f9e6e51e0
khoirulromadhon <khoirulromadhon1112@gmail.com>
2021-06-03 20:08:42 +0700

    menyisipkan template

--- database/seeders/UserBaruSeeder.php
+++ database/seeders/UserBaruSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
+class UserBaruSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -16,8 +16,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Administrator',
-            'email' => 'admin@admin.com',
+            'username' => 'admin',
+            'name' => 'admin.baru',
+            'email' => 'admin.baru@admin.com',
             'password' => Hash::make('password'),
         ]);
     }
